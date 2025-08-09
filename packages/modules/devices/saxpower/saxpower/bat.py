ee6d238ed110b37fc8be18efb021ccb8cf9b5673
SeaSpotter <florian.horch@gmail.com>
2025-08-09 20:53:20 +0200

    simplify bat reading

--- packages/modules/devices/saxpower/saxpower/bat.py
+++ packages/modules/devices/saxpower/saxpower/bat.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from typing import TypedDict, Any
 
-from modules.common import modbus
 from modules.common.abstract_device import AbstractBat
 from modules.common.component_state import BatState
 from modules.common.component_type import ComponentDescriptor
 from modules.common.fault_state import ComponentInfo, FaultState
-from modules.common.modbus import ModbusDataType
+from modules.common.modbus import ModbusDataType, ModbusTcpClient_
 from modules.common.simcount import SimCounter
 from modules.common.store import get_bat_value_store
 from modules.devices.saxpower.saxpower.config import SaxpowerBatSetup
@@ -14,8 +13,7 @@ from modules.devices.saxpower.saxpower.config import SaxpowerBatSetup
 
 class KwargsDict(TypedDict):
     device_id: int
-    client: modbus.ModbusTcpClient_
-    modbus_id: int
+    client: ModbusTcpClient_
 
 
 class SaxpowerBat(AbstractBat):
@@ -25,17 +23,16 @@ class SaxpowerBat(AbstractBat):
 
     def initialize(self) -> None:
         self.__device_id: int = self.kwargs['device_id']
-        self.__tcp_client: modbus.ModbusTcpClient_ = self.kwargs['client']
+        self.__tcp_client: ModbusTcpClient_ = self.kwargs['client']
         self.__modbus_id: int = self.kwargs['modbus_id']
         self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="speicher")
         self.store = get_bat_value_store(self.component_config.id)
         self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
 
     def update(self) -> None:
-        with self.__tcp_client:
-            # Die beiden Register müssen zwingend zusammen ausgelesen werden, sonst scheitert die zweite Abfrage.
-            soc, power = self.__tcp_client.read_holding_registers(46, [ModbusDataType.INT_16]*2, unit=self.__modbus_id)
-            power = power * -1 + 16384
+        unit = self.component_config.configuration.modbus_id
+        soc, power = self.__tcp_client.read_holding_registers(46, [ModbusDataType.INT_16]*2, unit=unit)
+        power = -power + 16384
 
         imported, exported = self.sim_counter.sim_count(power)
         bat_state = BatState(
