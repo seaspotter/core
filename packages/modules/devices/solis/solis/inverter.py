0b05636747f14aa2f32b3b4a5824cfabf7e5eb52
SeaSpotter <florian.horch@gmail.com>
2026-04-12 13:46:14 +0200

    Add SimCounter import to inverter.py

--- packages/modules/devices/solis/solis/inverter.py
+++ packages/modules/devices/solis/solis/inverter.py
@@ -5,6 +5,7 @@ from modules.common.component_state import InverterState
 from modules.common.component_type import ComponentDescriptor
 from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.modbus import ModbusDataType, ModbusTcpClient_
+from modules.common.simcount import SimCounter
 from modules.common.store import get_inverter_value_store
 from modules.devices.solis.solis.config import SolisInverterSetup
 from modules.devices.solis.solis.version import SolisVersion
