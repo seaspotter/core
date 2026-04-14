5fcbf47aa54d8210ec4d63cd6009d64f8090f675
SeaSpotter <florian.horch@gmail.com>
2026-04-14 12:04:48 +0200

    flake8

--- packages/modules/devices/anker/anker_solix/config.py
+++ packages/modules/devices/anker/anker_solix/config.py
@@ -38,7 +38,7 @@ class AnkerBatSetup(ComponentSetup[AnkerBatConfiguration]):
 
 
 class AnkerCounterConfiguration:
-    def __init__(self, modbus_id: int = 1
+    def __init__(self, modbus_id: int = 1,
                 ip_address: Optional[str] = None, port: int = 502):
         self.modbus_id = modbus_id
         self.ip_address = ip_address
